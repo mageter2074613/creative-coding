6def35d484e6d54d12285940d06fc64c3d320cd8
malachigeter <mgeter@mail.umw.edu>
2023-05-26 13:22:44 -0400

    quilt

--- week2/2.4.quilt-pattern/sketch.js
+++ week2/2.4.quilt-pattern/sketch.js
@@ -1,7 +1,43 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800); 
+  noLoop();
 }
 
 function draw() {
-  background(220);
+  background(255);
+  angleMode(DEGREES);
+
+  
+
+  // Loop for tiling the quilt pattern
+  for (let x = 0; x < 10; x++) {
+    for (let y = 0; y < 10; y++) {
+      push();
+      translate(x * 100, y * 100);
+
+      // Tile drawing code here
+      if ((x + y) % 2 === 0) {
+        
+        drawTileA();
+      } else {
+        
+        drawTileB();
+      }
+
+      pop();
+    }
+  }
 }
+
+function drawTileA() {
+
+  fill(255, 0, 0); // Red color
+  rect(0, 0, 100, 100); // Square tile
+}
+
+function drawTileB() {
+  
+  fill(0, 0, 255); // Blue color
+  ellipse(50, 50, 80, 80); // Circle tile
+}
+
