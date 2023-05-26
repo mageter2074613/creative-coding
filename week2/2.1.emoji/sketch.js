7b1e2767eccea98236e444870b7a07ee3bf4af63
malachigeter <mgeter@mail.umw.edu>
2023-05-26 12:38:57 -0400

    emoji
    fixed

--- week2/2.1.emoji/sketch.js
+++ week2/2.1.emoji/sketch.js
@@ -4,4 +4,18 @@ function setup() {
 
 function draw() {
   background(220);
+
+  stroke("black");
+strokeWeight(6);
+
+fill("yellow");
+ellipse(200,200,250,250);
+
+fill("black");
+ellipse(155,150,25,75);
+ellipse(245,150,25,75);
+
+ellispe(200,260,120,40);
+
+
 }
