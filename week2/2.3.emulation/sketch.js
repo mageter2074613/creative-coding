86c7156805de61910bf7aa25bb1869435edf2245
malachigeter <mgeter@mail.umw.edu>
2023-05-26 13:51:46 -0400

    emulation

--- week2/2.3.emulation/sketch.js
+++ week2/2.3.emulation/sketch.js
@@ -1,7 +1,35 @@
+// Artwork Emulation: Robert Mallary
+// Emulating the artwork "3-Color Computer Graphic"
+// Link: http://dada.compart-bremen.de/item/artwork/742
+
 function setup() {
-  createCanvas(400, 400);
+  
+  createCanvas(600, 600); 
+  
+  background("#ffffff"); 
+  
+
+  noLoop();
 }
 
 function draw() {
-  background(255);
+  // the "3-Color Computer Graphic" artwork
+  
+  var color1 = color("#ff0000"); // Red
+  var color2 = color("#00ff00"); // Green
+  var color3 = color("#0000ff"); // Blue
+  
+  // Divide into a grid
+  var gridSize = 20;
+  for (var x = 0; x < width; x += gridSize) {
+    for (var y = 0; y < height; y += gridSize) {
+      // a random color from the colors
+      var randomColor = random([color1, color2, color3]);
+      
+      // Draw rectangle with the color
+      fill(randomColor);
+      rect(x, y, gridSize, gridSize);
+    }
+  }
 }
+
