c4362c4b373229ea05f85a24c7b6b080e6443bd7
malachigeter <mgeter@mail.umw.edu>
2023-05-26 13:04:15 -0400

    circle grid

--- week2/2.2.circles/sketch.js
+++ week2/2.2.circles/sketch.js
@@ -1,7 +1,24 @@
 function setup() {
-  createCanvas(1200, 1200);
+  createCanvas(900, 900);
+  noLoop();
 }
 
 function draw() {
   background(220);
+  translate(100, 100);
+
+  for (let x = 0; x < 8; x++) {
+    for (let y = 0; y < 8; y++) {
+      push();
+      translate(x * 100, y * 100);
+
+      // Modify the circles using functions like stroke(), fill(), strokeWeight()
+      stroke(0);
+      fill(255);
+      strokeWeight(2);
+      ellipse(0, 0, 100, 100);
+
+      pop();
+    }
+  }
 }
