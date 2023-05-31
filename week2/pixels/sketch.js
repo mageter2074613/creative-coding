d63aee4c95b1ac688a4d132e146c38203ec19196
malachigeter <mgeter@mail.umw.edu>
2023-05-31 13:04:22 -0400

    Update sketch.js

--- week2/pixels/sketch.js
+++ week2/pixels/sketch.js
@@ -1,7 +1,18 @@
+
+ let img;
+
+function preload() {
+  img = loadImage(mageter2074613.github.io/creative-coding/week2/pixels/desert-ge74263ace_1280.jpg);
+}
+
 function setup() {
   createCanvas(800, 800);
+  img.loadPixels();
+  
 }
 
 function draw() {
   background(220);
 }
+
+// Additional functions and code
