80ba70a88b6f32bd23fcfcd60f0f5225fd3020e6
malachigeter <mgeter@mail.umw.edu>
2023-06-15 15:00:22 -0400

    Update sketch.js

--- week5/final/sketch.js
+++ week5/final/sketch.js
@@ -1,7 +1,72 @@
+// Global variables
+let audio;
+let video;
+let particles = [];
+
+function preload() {
+  // Load any necessary assets or data here
+  audio = loadSound('path/to/audio.mp3');
+  video = createVideo('path/to/video.mp4');
+}
+
 function setup() {
-  createCanvas(800, 800);
+  // Set up the canvas and any other initializations
+  createCanvas(windowWidth, windowHeight);
+
+  // Set up the video element
+  video.size(width, height);
+  video.loop();
+  video.hide();
+
+  // Set up the audio element
+  audio.loop();
+
+  // Create particles or other interactive elements
+  for (let i = 0; i < 100; i++) {
+    let particle = new Particle(random(width), random(height));
+    particles.push(particle);
+  }
 }
 
 function draw() {
-  background(220);
+  // Clear the canvas or apply background
+  background(0);
+
+  // Update and display particles
+  for (let i = 0; i < particles.length; i++) {
+    particles[i].update();
+    particles[i].display();
+  }
+
+  // Other visual effects or interactions
+  // ...
+
+  // Display video frame
+  image(video, 0, 0, width, height);
+}
+
+// Event handlers or interactive functionality
+function mousePressed() {
+  // Perform actions when the mouse is pressed
+  // ...
+}
+
+// Particle class for interactive elements
+class Particle {
+  constructor(x, y) {
+    this.position = createVector(x, y);
+    this.velocity = createVector(random(-1, 1), random(-1, 1));
+    this.color = color(random(255), random(255), random(255));
+  }
+
+  update() {
+    // Update the particle's position and velocity
+    this.position.add(this.velocity);
+  }
+
+  display() {
+    // Display the particle on the canvas
+    fill(this.color);
+    ellipse(this.position.x, this.position.y, 10);
+  }
 }
