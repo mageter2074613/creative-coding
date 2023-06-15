2d8479a5bdabafd8d8e69d2034ac86e37e7f3c3f
malachigeter <mgeter@mail.umw.edu>
2023-06-15 14:50:44 -0400

    Update sketch.js

--- week4/book/sketch.js
+++ week4/book/sketch.js
@@ -1,3 +1,57 @@
-function setup() {
+const PDFDocument = require('pdfkit');
+const fs = require('fs');
 
+// Load the text content from a file or URL
+function loadTextContent(url) {
+  // Use appropriate method to load the text content (e.g., loadStrings, readFile, etc.)
+  // Return the loaded text content as a single string or an array of lines
 }
+
+// Generate the book using the text content
+function generateBook(content) {
+  const doc = new PDFDocument();
+  doc.pipe(fs.createWriteStream('book.pdf'));
+
+  // Set the font and font size for the book
+  doc.font('path/to/font.ttf').fontSize(12);
+
+  // Calculate the number of words written
+  let wordCount = 0;
+
+  // Write the content to the PDF document
+  for (let i = 0; i < content.length; i++) {
+    const line = content[i];
+    const words = line.split(' ');
+
+    for (let j = 0; j < words.length; j++) {
+      const word = words[j];
+      doc.text(word + ' ');
+
+      // Increase the word count
+      wordCount++;
+
+      // Check if the word count exceeds the target
+      if (wordCount >= 50000) {
+        // End the PDF document
+        doc.end();
+        return;
+      }
+    }
+
+    // Add a new line after each line of text
+    doc.moveDown();
+  }
+
+  // End the PDF document
+  doc.end();
+}
+
+// Preload the text content
+function preload() {
+  const url = 'https://mageter2074613.github.io/creative-coding'; // Replace with the URL of your text content
+  const content = loadTextContent(url);
+  generateBook(content);
+}
+
+// Start the preloading process
+preload();
