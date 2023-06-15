f653fefd745a6aeece4f30b7e59ec1b58acd5f7d
malachigeter <mgeter@mail.umw.edu>
2023-06-15 14:11:30 -0400

    Love, Markov

--- week4/4.3.love-markov/sketch.js
+++ week4/4.3.love-markov/sketch.js
@@ -1,7 +1,36 @@
+let rm = RiTa.markov(2);
+let data;
+
 function setup() {
-  createCanvas(800, 800);
+  noCanvas();
+  let sourceElement = select("#source");
+  data = sourceElement.elt.innerText;
+  rm.addText(data);
+
+  let letter = select("#letter");
+  letter.child(createP('My Dearest Harrington,'));
+
+  let lines = rm.generate(6);
+  for (let l = 0; l < lines.length; l++) {
+    let paragraph = createP(lines[l]);
+    letter.child(paragraph);
+  }
+
+  letter.child(createP('Sincerely,<br>Markov'));
 }
 
-function draw() {
-  background(220);
+function mousePressed() {
+  let letter = select("#letter");
+  letter.remove();
+  letter = createDiv();
+  letter.id("letter");
+  letter.child(createP('My Dearest Harrington,'));
+
+  let lines = rm.generate(6);
+  for (let l = 0; l < lines.length; l++) {
+    let paragraph = createP(lines[l]);
+    letter.child(paragraph);
+  }
+
+  letter.child(createP('Sincerely,<br>Markov'));
 }
