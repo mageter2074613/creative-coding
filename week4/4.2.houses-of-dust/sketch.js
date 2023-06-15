6a0f7ffe563fa384c77d5181b0539484e5a2e7cc
malachigeter <mgeter@mail.umw.edu>
2023-06-15 13:58:20 -0400

    Update sketch.js

--- week4/4.2.houses-of-dust/sketch.js
+++ week4/4.2.houses-of-dust/sketch.js
@@ -1,7 +1,24 @@
+let poem;
+let grammar = tracery.createGrammar({
+   "origin": "A HOUSE OF #material#\n#place#\nUSING #light_source#\nINHABITED BY #inhabitants#",
+   "material": ["Bricks", "Glass", "Wood", "Concrete", "Steel"],
+   "place": ["On a hill", "By the sea", "In the forest", "In the city", "In the desert"],
+   "light_source": ["Sunlight", "Lamps", "Candles", "Fire", "LEDs"],
+   "inhabitants": ["Dreamers", "Artists", "Wanderers", "Spirits", "Creatures"]
+});
+
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(400, 400);
+  noLoop(); // Disable the drawing loop
 }
 
 function draw() {
   background(220);
+  
+  for (let i = 0; i < 4; i++) { // Generate four quattrains
+    poem = grammar.flatten("#origin#");
+    textSize(16);
+    textAlign(CENTER);
+    text(poem, width/2, 50 + i*100); // Adjust y position to fit multiple quattrains
+  }
 }
