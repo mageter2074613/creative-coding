642c3bf7cbd10fabd9f7d0189343a80576ede0e9
malachigeter <mgeter@mail.umw.edu>
2023-06-16 16:00:28 -0400

    l

--- week3/3.1.bounce/sketch.js
+++ week3/3.1.bounce/sketch.js
@@ -1,50 +1,39 @@
-let cx = 0; 
-let cy = 0; 
-
-// width and height of circle
-let cw = 40; 
-let ch = 40; 
-
-// horizontal and vertical speeds
-let hspeed = Math.random() * 40;
-let vspeed = Math.random() * 40;
-
-// how much stretching to do
-let vstretch = 30;
-let hstretch = 30;
+let x = 0;
+let y = 0;
+let xspeed = 1;
+let yspeed = 1;
 
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(50); 
-  noStroke();
+  background(220);
 
-  // circle color
-  fill(250);
+  // Check for collision with left and right edge
+  if (x < 0 || x > width) {
+    xspeed *= -1; // Reverse the x direction
+  }
 
-  // shift drawing plane to the center of the sketch
-  translate(400,400);
-  
-  // check for horizontal collisions, reverse h direction
-  if (cx > width/2 - 40 || cx < -width/2 + 40){
-    hspeed = -hspeed;
+  // Check for collision with top and bottom edge
+  if (y < 0 || y > height) {
+    yspeed *= -1; // Reverse the y direction
   }
-  
-  // check for vertical collisions, reverse v direction
-  if (cy > height/2 - 40 || cy < -height/2 + 40){
-    vspeed = -vspeed;
-  }
-  
-  // adjust the x and y positions by the h and v factors
-  cx += hspeed;
-  cy += vspeed;
-  
-  // checking for nearness to the x edges
-  let reach = ch;
-  if (abs(cx) > 110){
-    // adjust the stretch linearly as circle approaches edge
-    reach = map(abs(cx),110,160,ch, ch + vstretch)
-  }
+
+  x += xspeed; // Iterate x
+  y += yspeed; // Iterate y
+
+  // Draw a circle
+  circle(x, y, 50);
+}
+
+function mouseClicked() {
+  // Add a new bouncing object at the mouse position
+  let newObject = {
+    x: mouseX,
+    y: mouseY,
+    xspeed: random(-2, 2),
+    yspeed: random(-2, 2)
+  };
+  objects.push(newObject);
 }
