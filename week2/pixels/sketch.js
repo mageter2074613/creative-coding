903454633c2929c948a956db705146fb9d9c7bad
malachigeter <mgeter@mail.umw.edu>
2023-06-16 16:25:36 -0400

    Update sketch.js

--- week2/pixels/sketch.js
+++ week2/pixels/sketch.js
@@ -1,7 +1,7 @@
 
 let img;
 function preload(){
-  img = loadImage('https://mageter2074613.github.io/creative-coding/week2/pixels/desert-ge74263ace_1280.jpg');
+  img = loadImage('c:\Users\malac\Downloads\desert-ge74263ace_1280.jpg');
 }
 //mageter2074613.github.io/creative-coding/week2/pixels/desert-ge74263ace_1280.jpg
 function setup() {
@@ -12,6 +12,25 @@ function setup() {
 
 function draw() {
   background(220);
-}
 
+  // Manipulate and display the image pixels here
+  for (let x = 0; x < img.width; x++) {
+    for (let y = 0; y < img.height; y++) {
+      // Get the color of the pixel at (x, y)
+      let pixelColor = img.get(x, y);
+
+      // Example manipulation: invert the pixel color
+      let invertedColor = color(255 - red(pixelColor), 255 - green(pixelColor), 255 - blue(pixelColor));
+
+      // Set the manipulated color to the pixel at (x, y)
+      img.set(x, y, invertedColor);
+    }
+  }
+
+  // Update the modified image pixels
+  img.updatePixels();
+
+  // Display the modified image
+  image(img, 0, 0);
+}
 // Additional functions and code
